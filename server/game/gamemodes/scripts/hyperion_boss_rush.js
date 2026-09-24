409aee51257367acf0a787aa4693cebe00a058cc
Rodrigo of Rodrigonia <rodrigo.rodrigues.rr.acc2@gmail.com>
2026-09-24 13:28:42 +0100

    MORE HYPERIONS

--- server/game/gamemodes/scripts/hyperion_boss_rush.js
+++ server/game/gamemodes/scripts/hyperion_boss_rush.js
@@ -9,7 +9,7 @@ class HyperionBossRush  {
         [[6,"hyperion_mk1f"]],
         [[2,"hyperion_mk1r"],[2,"hyperion_mk1g"],[2,"hyperion_mk1b"]],
         [[4,"not_crius_mk1"],[3,"hyperion_mk1r"]],
-        [[12,"hyperion_mk0"]],
+        ran.choose([[[12,"hyperion_mk0"]],[[2,"hyperion_mk2"]],[[6,"hyperion_mk1"]]]),
         ran.choose([[[1,"hyperion_mk2r"],[2,"hyperion_mk1r"]],[[1,"hyperion_mk2g"],[2,"hyperion_mk1g"]],[[1,"hyperion_mk2b"],[2,"hyperion_mk1b"]],[[3,"hyperion_mk1r"],[3,"hyperion_mk1g"],[3,"hyperion_mk1b"]]])];
     }
     defineProperties() {
