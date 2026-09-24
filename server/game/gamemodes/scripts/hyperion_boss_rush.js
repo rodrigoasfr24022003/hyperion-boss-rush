080b5ded45e521cad6134355c45475ae5747b16e
Rodrigo of Rodrigonia <rodrigo.rodrigues.rr.acc2@gmail.com>
2026-09-24 13:22:03 +0100

    waves

--- server/game/gamemodes/scripts/hyperion_boss_rush.js
+++ server/game/gamemodes/scripts/hyperion_boss_rush.js
@@ -10,7 +10,7 @@ class HyperionBossRush  {
         [[2,"hyperion_mk1r"],[2,"hyperion_mk1g"],[2,"hyperion_mk1b"]],
         [[4,"not_crius_mk1"],[3,"hyperion_mk1r"]],
         [[12,"hyperion_mk0"]],
-        ran.choose([[[1,"hyperion_mk2b"],[2,"hyperion_mk1b"]],[[1,"hyperion_mk2r"],[2,"hyperion_mk1r"]],[[3,"hyperion_mk1r"],[3,"hyperion_mk1g"],[3,"hyperion_mk1b"]]])];
+        ran.choose([[[1,"hyperion_mk2r"],[2,"hyperion_mk1r"]],[[1,"hyperion_mk2g"],[2,"hyperion_mk1g"]],[[1,"hyperion_mk2b"],[2,"hyperion_mk1b"]],[[3,"hyperion_mk1r"],[3,"hyperion_mk1g"],[3,"hyperion_mk1b"]]])];
     }
     defineProperties() {
         this.length = Config.wave_cap;
